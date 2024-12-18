e7a947e5cc9da2b9794945e352552063a95de087
Jorge Verdugo <jorgeverdugo3005@gmail.com>
2024-12-17 17:23:59 -0700

    Initial Commit

---
+++ ImageZoomTransition/ImageZoomTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageZoomTransitionApp.swift
+//  ImageZoomTransition
+//
+//  Created by Jorge Verdugo on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageZoomTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
